d61caf4b621cc2ecec8fbb9976b7ee289e57e873
deepakchauhan33 <deep651965@gmail.com>
2026-02-05 13:36:19 +0530

    Add content in the product Page

--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -9,7 +9,7 @@ const ProductPage = () => {
     const params = useParams();
     const [product, setProduct] = useState(null);
 
-    
+
 
 
     useEffect(() => {
@@ -31,9 +31,89 @@ const ProductPage = () => {
     }
 
 
+    function plusPercentageValue(num){
+
+        return 0.1*num + num;
+    }
+
+
+
+    function warrantyInfo(category){
+
+        if(category === "electronics"){
+            return "2 years warranty";
+        }else if(category === "jewelery"){
+            return "1 year warranty";
+        }else if(category === "men's clothing" || category === "women's clothing"){
+            return "6 month warranty";
+        }else{
+            return "No warranty information available";
+        }
+    }
+
+
+    function handleRating(rate) {
+        let stars = '';
+        for (let i = 1; i <= 5; i++) {
+            if (i <= Math.floor(rate)) {
+                stars += '★';
+            } else {
+                stars += '☆';
+            }
+        }
+        return stars;
+    }
+
+
     return (
         <>
-            {product ? <p>{product.title}</p> : <Loader/>}
+            {product ? <p>{
+
+                <div className='flex justify-center p-10'>
+
+                    <div className='w-1/3 border'>
+                        <img src={product.image} alt={product.title} className='bg-gray-300 p-4' />
+                    </div>
+
+                    <div className='flex flex-col w-full border p-4'>
+                        <p className='text-2xl font-semibold'>
+                            {product.title}
+                        </p>
+
+                        <span>{product.rating.rate} {`(${product.rating.count} reviews)`}</span> <span>{handleRating(product.rating.rate)}</span>
+
+
+                        <p>
+                            {product.category}
+                        </p>
+                        <p>
+                            {product.description}
+                        </p>
+
+                        <div className='space-x-2'>
+
+                            <span className='text-red-500'>
+                                -10%
+                            </span>
+
+                            <span className='text-2xl font-bold'>
+                                ${product.price}
+                            </span>
+
+                            <p className='line-through'>{`M.R.P : $ ${plusPercentageValue(product.price)} `}</p>
+
+
+                            <p>
+                                {warrantyInfo(product.category)}
+                            </p>
+
+                        </div>
+                    </div>
+
+
+                </div>
+
+            }</p> : <Loader />}
         </>
     )
 }
