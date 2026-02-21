57e04a438aab499baf55758580bd14b027b18f1a
deepakchauhan33 <deep651965@gmail.com>
2026-02-21 21:39:36 +0530

    Create contact page form

--- src/Pages/Contact/ContactFrom.jsx
+++ src/Pages/Contact/ContactFrom.jsx
@@ -2,21 +2,11 @@ import React from 'react'
 
 const ContactFrom = () => {
     return (
-        <div>
 
-            {/* Contact Form */}
-            <from>
-
-                <div className='flex flex-col'>
-                    <label htmlFor="Name">First Name</label>
-                    <input
-                        type="text"
-                        placeholder='Enter your name'
-                        name='Name'
-                        id='FirstName'
-                    />
-                </div>
-
+            
+            <from 
+                className="w-1/2 border-2 border-gray-300 rounded-lg p-5 flex flex-col gap-5"
+            >
 
                 <div className='flex flex-col'>
                     <label htmlFor="Name">First Name</label>
@@ -30,41 +20,43 @@ const ContactFrom = () => {
 
 
                 <div className='flex flex-col'>
-                    <label htmlFor="Name">First Name</label>
+                    <label htmlFor="email">Email</label>
                     <input
-                        type="text"
-                        placeholder='Enter your name'
-                        name='Name'
-                        id='FirstName'
+                        type="email"
+                        placeholder='Enter your email'
+                        name='email'
+                        id='email'
                     />
                 </div>
 
 
                 <div className='flex flex-col'>
-                    <label htmlFor="Name">First Name</label>
+                    <label htmlFor="number">Phone Number</label>
                     <input
                         type="text"
-                        placeholder='Enter your name'
-                        name='Name'
-                        id='FirstName'
+                        placeholder='Enter your phone number'
+                        name='number'
+                        id='number'
                     />
                 </div>
 
 
                 <div className='flex flex-col'>
-                    <label htmlFor="Name">First Name</label>
-                    <input
-                        type="text"
-                        placeholder='Enter your name'
-                        name='Name'
-                        id='FirstName'
+                    <label htmlFor="message">Message</label>
+                    <textarea
+                        placeholder='Enter your message'
+                        name='message'
+                        id='message'
                     />
                 </div>
 
 
+                
+
+                <button className='bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600'>Submit</button>
 
             </from>
-        </div>
+
     )
 }
 
